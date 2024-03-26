b0e05a330052289ef2a03aa5fe178eb37984df38
EunjiKim22 <88539014+EunjiKim22@users.noreply.github.com>
2024-03-26 23:27:07 +0900

    update

--- vehicle.py
+++ vehicle.py
@@ -1,20 +1,23 @@
 class Vehicle:
-    def __init__(self, color, doors, tires):
+    def __init__(self, color, doors, tires, vtype):
         self.color = color
         self.doors = doors
         self.tires = tires
+        self.vtype = vtype
 
     def brake(self):
         #Stop the car
-        return "Braking"
-
+        return "%sBraking" % self.vtype
+ 
     def drive(self):
         #Drive the car
-        return "I'm driving!"
+        return "I'm driving a %s %s!" % (self.color, self.vtype)
 
 if __name__=="__main__":
-    car = Vehicle("blue", 5, 4)
-    print(car.color)
+    car = Vehicle("blue", 5, 4, "car")
+    print(car.brake())
+    print(car.drive())
 
     truck = Vehicle("red", 3, 6)
-    print(truck.color)
+    print(truck.brake())
+    print(truck.drive)
